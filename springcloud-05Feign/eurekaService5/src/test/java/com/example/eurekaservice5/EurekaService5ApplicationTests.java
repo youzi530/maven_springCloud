9af6f04a153486c539909c2a3e3b37ab5d518b47
lingqiao <2676272310@qq.com>
2021-11-22 11:06:09 +0800

    [lingqiao]feat:add the eurekaService

---
+++ springcloud-05Feign/eurekaService5/src/test/java/com/example/eurekaservice5/EurekaService5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaservice5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaService5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
